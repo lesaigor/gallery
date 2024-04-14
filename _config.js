7cc0ebd3179657bf5bf1dca396a82f87d524b35e
lesaigor <pridelesaigor@gmail.com>
2024-04-14 17:25:30 +0300

    updated test config file

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
-var config = {}
+var config = {};
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+    production: 'mongodb+srv://pridelesaigor:D5bPkHfkzBFrvLBg@lesaigor.ufi93xd.mongodb.net/your-database-name?retryWrites=true&w=majority&appName=Lesaigor',
+    development: 'mongodb+srv://pridelesaigor:D5bPkHfkzBFrvLBg@lesaigor.ufi93xd.mongodb.net/your-database-name?retryWrites=true&w=majority&appName=Lesaigor',
+    test: 'mongodb+srv://pridelesaigor:D5bPkHfkzBFrvLBg@lesaigor.ufi93xd.mongodb.net/your-database-name?retryWrites=true&w=majority&appName=Lesaigor'
+};
+
 module.exports = config;
